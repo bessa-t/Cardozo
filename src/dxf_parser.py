b9023efd115ee3dd139d211457fa81f6b0ab324f
Tarso Bessa <bessatarso@gmail.com>
2025-08-22 22:06:02 -0300

    stable version of dxf_parser with future improvements

--- src/dxf_parser.py
+++ src/dxf_parser.py
@@ -2,7 +2,6 @@
 # OOP implementation of a DXF parser with built-in normalization.
 
 import ezdxf
-from typing import List
 
 class DXFParser:
     """
@@ -12,24 +11,21 @@ class DXFParser:
         """Initializes the parser, loads the doc, and finds the geometry offset."""
         try:
             self.doc = ezdxf.readfile(dxf_filepath)
-        except:
+        except (FileNotFoundError, ezdxf.DXFError) as e:
             raise ValueError(f"Could not read DXF file: {dxf_filepath}!")
         else:
             print(f"DXF file '{dxf_filepath}' loaded successfully.")
 
         self.msp = self.doc.modelspace()
 
-        self.polylines = [] 
-        self.circles = [] 
-        self.concrete = []
-        self.steel_bars = []
 
     def parse(self):
         """Extracts and normalizes geometry from the DXF file."""
+        self.concrete = []
+        self.steel_bars = []
         # Query entities by layer names
         polyline_query = self.msp.query('LWPOLYLINE')
         circle_query = self.msp.query('CIRCLE')
-
         for polyline in polyline_query:
             layer = polyline.dxf.layer.lower()
             if not polyline.is_closed:
@@ -44,7 +40,9 @@ class DXFParser:
                 raise ValueError("Steel bars should be represented as circles, not polylines.")
             if  layer != "concrete" and layer != "steel bars":
                 raise ValueError("Invalid layer found. Required layers are 'concrete' or 'steel bars'.")
-            
+               
+        if len(self.concrete) >2:
+            raise ValueError("More than two polylines found. Only concrete sections with one hole are allowed.")
         for circle in circle_query:
             layer = circle.dxf.layer.lower()
 
@@ -59,9 +57,12 @@ class DXFParser:
                 self.steel_bars.append(circle_data)
 
             if layer == "concrete":
-                self.concrete.append(circle_data)
-        print("=======================================================")
-        print(self.concrete)
-        print(len(self.concrete))
+                raise ValueError("Concrete sections should be represented as polylines, not circles.")
+        return self.concrete, self.steel_bars
+    
 test = DXFParser("/home/tarso/projects/biaxal_bending/Biaxial-Bending-Diagram/dxf_files/section 7.dxf")
-test.parse()
+print(test.parse())
+# Future improvements:
+# - Cases with multiple holes
+# - Section doesnt have to be pre-normalized
+# - More robust error handling and reporting
