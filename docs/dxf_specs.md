cc36eb3bd420fbc036b1809b5b9301bbc7a022d5
Tarso Bessa <bessatarso@gmail.com>
2026-01-12 14:39:11 -0300

    refactor: restructure project layout and update DXF specs
    - Move source code to 'src/' package to isolate application logic
    
    - Separate tests into 'tests/' directory for better discovery
    
    - Add '__init__.py' files to establish proper Python packages
    
    - Move and rename requirements to 'docs/dxf_specs.md'
    
    - Define strict geometric constraints for concrete and steel layers

---
+++ docs/dxf_specs.md
@@ -0,0 +1,4 @@
+concrete layer has to be nammed as "concrete"
+concrete section have to be closed polylines
+steel bars layer have to be nammed as "steel bars"
+steel bars have to be circles
--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from materials import concrete, steel
 parser = DXFParser("/home/tarso/projects/biaxal_bending/Biaxial-Bending-Diagram/dxf_files/section 7.dxf")
 builder = GeometryBuilder()
 section= builder.build_section(parser.parse(),concrete,steel)
-
+#section.plot_section()
 #testando
 
 bb_res =section.biaxial_bending_diagram(n_points=24, progress_bar=False)
@@ -16,3 +16,4 @@ bb_res =section.biaxial_bending_diagram(n_points=24, progress_bar=False)
 bb_res.plot_diagram(eng=True)
 
 plt.show()
+
